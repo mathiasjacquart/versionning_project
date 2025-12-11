6db19e24eb17567459d7d79d0e9cbd6b7060ce19
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2025-12-11 12:37:32 +0100

    feat(login): ajout des variables d'environnement

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
-// Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2lqV7aHskJ2LQqG5k-l3l9LrVQPE4f9I",
-  authDomain: "versionning-project.firebaseapp.com",
-  projectId: "versionning-project",
-  storageBucket: "versionning-project.firebasestorage.app",
-  messagingSenderId: "587581056108",
-  appId: "1:587581056108:web:c6ebb27122d3d634956449",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Exports
 export const auth = getAuth(app);
 export const storage = getStorage(app);
